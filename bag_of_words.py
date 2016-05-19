992bef7d2d11aa3e52eaf5ad2680a012bbb4cede
pierre <pierre.vigier@ymail.com>
2016-05-20 00:59:05 +0200

    Update the report.

--- bag_of_words.py
+++ bag_of_words.py
@@ -1,3 +1,5 @@
+import pickle
+import os
 from sklearn.feature_extraction.text import CountVectorizer
 from util import *
 
@@ -11,6 +13,9 @@ def get_features(reviews, vectorizer):
     print('Creating bag of words...')
     data_features = vectorizer.transform(reviews)
     return data_features.toarray()
+
+def get_word_features(word, vectorizer):
+    return vectorizer.transform([word]).toarray().flatten()
 
 def predict(algorithm='rf'):
     train = get_reviews('data/labeledTrainData.tsv')
@@ -37,4 +42,42 @@ def predict(algorithm='rf'):
 
     predict(test_data_features, test, classifier, 'results/bag_of_words_model_' + algorithm + '.csv')
 
-predict('rf')
+def get_words_weights():
+    filename = 'models/bow-lr'
+    if not os.path.exists(filename):
+        train = get_reviews('data/labeledTrainData.tsv')
+        clean_train_reviews = clean_reviews(train['review'], join_words=True)
+        vectorizer = train_vectorizer(clean_train_reviews)
+        train_data_features = get_features(clean_train_reviews, vectorizer)
+        classifier = train_logistic_regression(train_data_features, train)
+        with open(filename, 'wb') as f:
+            pickle.dump((vectorizer, classifier), f)
+        print('Free memory...')
+        del train
+        del clean_train_reviews
+        del train_data_features
+    else:
+        print('Training skipped - ' + filename + ' exists')
+
+    with open(filename, 'rb') as f:
+        vectorizer, classifier = pickle.load(f)
+        sentiments = classifier.coef_.flatten()
+        words = vectorizer.get_feature_names()
+        word2sentiment = dict(zip(words, sentiments))
+
+        n = 10
+        sorted_words = sorted(word2sentiment.items(), key=lambda x: x[1])
+        positive = reversed(sorted_words[-n:])
+        negative = sorted_words[:n]
+        print('NEGATIVE')
+        for i, (word, sentiment) in enumerate(negative):
+            print('{}. {} ({})'.format(i+1, word, sentiment))
+        print('\nPOSITIVE')
+        for i, (word, sentiment) in enumerate(positive):
+            print('{}. {} ({})'.format(i+1, word, sentiment))
+
+
+        tests = ['like', 'love', 'dislike', 'hate', 'amazing', 'awful']
+
+
+get_words_weights()
